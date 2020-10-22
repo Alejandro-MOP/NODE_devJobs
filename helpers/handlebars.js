4d2506fb31d4d682755a9d924765bd77dc0cb96b
Alejandro Montes de Oca <la.montesdeoca.p@gmail.com>
2020-10-21 20:47:54 -0500

    Nueva funcion
    Agregue un helper mas para poder seleccionar en automatico las opciones del select que viene desde la BD

--- helpers/handlebars.js
+++ helpers/handlebars.js
@@ -1,19 +1,25 @@
 module.exports = {
     seleccionarSkills: (seleccionadas = [], opciones) => {
-        const skills = ['HTML5', 'CSS3', 'CSSGrid', 'Flexbox',
-        'JavaScript', 'jQuery', 'Node', 'Angular', 'VueJS', 'ReactJS',
-        'React Hooks', 'Redux', 'Apollo', 'GraphQL', 'TypeScript', 'PHP',
-        'Laravel', 'Symfony', 'Python', 'Django', 'ORM', 'Sequelize',
-        'Mongoose', 'SQL', 'MVC', 'SASS', 'WordPress'
+        const skills = [
+            'HTML5', 'CSS3', 'CSSGrid', 'Flexbox',
+            'JavaScript', 'jQuery', 'Node', 'Angular', 'VueJS', 'ReactJS',
+            'React Hooks', 'Redux', 'Apollo', 'GraphQL', 'TypeScript', 'PHP',
+            'Laravel', 'Symfony', 'Python', 'Django', 'ORM', 'Sequelize',
+            'Mongoose', 'SQL', 'MVC', 'SASS', 'WordPress'
         ];
 
         let html = '';
         skills.forEach(skill => {
             html += `
-                <li>${skill}</li>
+                <li ${seleccionadas.includes(skill) ? 'class="activo"' : ''}>${skill}</li>
             `;
         });
 
         return opciones.fn().html = html;
+    },
+
+    //funcion para seleccionar automaticamente la opcion del select que viene desde la BD
+    tipoContrato: (seleccionado, opciones) => {
+        return opciones.fn(this).replace( new RegExp(` value="${seleccionado}"`), '$& selected="selected"' );
     }
 }

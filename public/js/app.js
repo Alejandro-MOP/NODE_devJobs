543296c82fdef5d3436886565bbdfe3e383bbb93
Alejandro Montes de Oca <la.montesdeoca.p@gmail.com>
2020-10-21 20:49:01 -0500

    Funcion para seleccionar skills
    Funcion para inyectar HTML con los skills de la vacante que vienen desde BD

--- public/js/app.js
+++ public/js/app.js
@@ -3,24 +3,38 @@ document.addEventListener('DOMContentLoaded', () => {
 
     if (skills) {
         skills.addEventListener('click', agregarSkills);
+
+        //editando, llamar la funcion para leer los datos previos del helper
+        skillsSeleccionados();
     }
 });
 
 const skills = new Set();
 
 const agregarSkills = e => {
-        // console.log(e.target)
-        if(e.target.tagName === 'LI'){
-            if(e.target.classList.contains('activo')){
-                skills.delete(e.target.textContent);
-                e.target.classList.remove('activo');
-            } else {
-                skills.add(e.target.textContent);
-                e.target.classList.add('activo');
-            }
+    // console.log(e.target)
+    if(e.target.tagName === 'LI'){
+        if(e.target.classList.contains('activo')){
+            skills.delete(e.target.textContent);
+            e.target.classList.remove('activo');
+        } else {
+            skills.add(e.target.textContent);
+            e.target.classList.add('activo');
         }
-        // console.log(skills);
-        const skillsArray = [...skills];
-        document.querySelector('#skills').value = skillsArray;
+    }
+    // console.log(skills);
+    const skillsArray = [...skills];
+    document.querySelector('#skills').value = skillsArray;
 
+}
+
+
+const skillsSeleccionados = () => {
+    const seleccionadas = Array.from(document.querySelectorAll('.lista-conocimientos .activo'));
+    //console.log(seleccionadas);
+
+    seleccionadas.forEach(seleccionada => {skills.add(seleccionada.textContent)})
+    //inyectarlo en el input hidden de editar vacante
+    const skillsArray = [...skills];
+    document.querySelector('#skills').value = skillsArray;
 }

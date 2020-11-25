4874ca3bc157d211128ef81508c22fc7fb78aadc
Alejandro Montes de Oca <la.montesdeoca.p@gmail.com>
2020-11-24 22:34:11 -0600

    Modifique los metodos asincronos para controlar el comportamiento con Try Catch

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,18 +1,21 @@
 const Vacante = require('../models/Vacantes');
 
 exports.mostrarTrabajos = async (req, res, next) => {
+    try {
+        const vacantes = await Vacante.find().lean();
 
-    const vacantes = await Vacante.find().lean();
+        if(!vacantes) return next();
 
-    if(!vacantes) return next();
+        res.status(200).render('home', {
+            nombrePagina: 'devJobs',
+            tagline: 'Encuentra y Pública Trabajos para Desarrolladores Web',
+            barra: true,
+            boton: true,
+            vacantes
+        });
 
-    res.render('home', {
-        nombrePagina: 'devJobs',
-        tagline: 'Encuentra y Pública Trabajos para Desarrolladores Web',
-        barra: true,
-        boton: true,
-        vacantes
-    });
+    } catch (error) {
+        console.error(`Hubo un error: ${error}`);
+    }
 
-    //console.log(vacantes);
 }
--- controllers/vacantesController.js
+++ controllers/vacantesController.js
@@ -11,50 +11,68 @@ exports.formularioNuevaVacante = (req, res) => {
 
 exports.agregarVacante = async (req, res) => {
 
-    const vacante = new Vacante(req.body);
+    try {
+        const vacante = new Vacante(req.body);
 
-    vacante.skills = req.body.skills.split(',');
-    // console.log(vacante);
+        vacante.skills = req.body.skills.split(',');
 
-    const nuevaVacante = await vacante.save();
+        const nuevaVacante = await vacante.save();
 
-    res.redirect(`/vacantes/${nuevaVacante.url}`);
+        res.status(201).redirect(`/vacantes/${nuevaVacante.url}`);
+
+    } catch (error) {
+        console.error(`Hubo un error: ${error}`);
+    }
 }
 
 exports.mostrarVacante = async (req, res, next) => {
 
-    const vacante = await Vacante.findOne({ url: req.params.url }).lean();
+    try {
+        const vacante = await Vacante.findOne({ url: req.params.url }).lean();
 
-    if(!vacante) return next();
+        if (!vacante) return next();
 
-    res.render('vacante',{
-        vacante,
-        nombrePagina: vacante.titulo,
-        barra: true
-    });
+        res.status(200).render('vacante', {
+            vacante,
+            nombrePagina: vacante.titulo,
+            barra: true
+        });
+
+    } catch (error) {
+        console.error(`Hubo un error: ${error}`);
+    }
 }
 
 
 exports.formEditarVacante = async (req, res, next) => {
+    try {
+        const vacante = await Vacante.findOne({ url: req.params.url }).lean();
 
-    const vacante = await Vacante.findOne({ url: req.params.url }).lean();
+        if (!vacante) return next();
 
-    if(!vacante) return next();
+        res.status(200).render('editar-vacante', {
+            vacante,
+            nombrePagina: `Editar - ${vacante.titulo}`
+        });
 
-    res.render('editar-vacante',{
-        vacante,
-        nombrePagina: `Editar - ${vacante.titulo}`
-    });
+    } catch (error) {
+        console.error(`Hubo un error: ${error}`);
+    }
 }
 
 exports.editarVacante = async (req, res) => {
+    try {
+        const vacanteActualizada = req.body;
 
-    const vacanteActualizada = req.body;
-    vacanteActualizada.skills = req.body.skills.split(',');
+        vacanteActualizada.skills = req.body.skills.split(',');
 
-    const vacante = await Vacante.findOneAndUpdate({url: req.params.url}, vacanteActualizada, {
-        new: true,
-        runValidators: true
-    });
-    res.redirect(`/vacantes/${vacante.url}`);
+        const vacante = await Vacante.findOneAndUpdate({ url: req.params.url }, vacanteActualizada, {
+            new: true,
+            runValidators: true
+        });
+        res.status(200).redirect(`/vacantes/${vacante.url}`);
+
+    } catch (error) {
+        console.error(`Hubo un error: ${error}`);
+    }
 }
